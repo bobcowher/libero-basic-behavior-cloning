b13abd883a189313f677ca0d3fb1c16deea21bfa
Robert Cowher <bobcowher@gmail.com>
2026-08-02 16:14:17 -0500

    exp: hidden_dim=256 compression_dim=256 n_hidden_layers=2

--- agent.py
+++ agent.py
@@ -108,7 +108,7 @@ class Agent:
         # hidden_dim=256, compression_dim=256 (tied), n_hidden_layers=1.
         self.hidden_dim = 256
         self.compression_dim = 256
-        self.n_hidden_layers = 1
+        self.n_hidden_layers = 2
         # -----------------------------------------------------------------
 
         self.model = Model(
